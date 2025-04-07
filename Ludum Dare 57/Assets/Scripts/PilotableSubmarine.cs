c9ee595c45f708d95cc11437d265d3194c6b411e
NuiN99 <endyverse@gmail.com>
2025-04-07 13:02:15 -0700

    feat: submarine movement

--- Ludum Dare 57/Assets/Scripts/PilotableSubmarine.cs
+++ Ludum Dare 57/Assets/Scripts/PilotableSubmarine.cs
@@ -6,9 +6,15 @@ public class PilotableSubmarine : MonoBehaviour, IInteractable
     [SerializeField] Rigidbody rb;
     [SerializeField] Collider col;
 
+    [Header("Movement")] 
+    [SerializeField] float moveSpeed;
+    
+    [SerializeField] float turnTorque = 50f;
+    [SerializeField] float maxAngularSpeed = 1f;
+    
     bool _isActive;
 
-    void Awake()
+    void Start()
     {
         col.enabled = false;
         rb.isKinematic = true;
@@ -31,5 +37,55 @@ public class PilotableSubmarine : MonoBehaviour, IInteractable
         rb.isKinematic = false;
         _isActive = true;
         player.gameObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        if (!_isActive)
+        {
+            return;
+        }
+        
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = GetMovementDirection();
+        if (moveDir == Vector3.zero) return;
+
+        rb.AddForce(moveDir * moveSpeed, ForceMode.Acceleration);
+    }
+
+    
+    void Rotate()
+    {
+        
+        Vector2 rotateInput = InputManager.RotateInput;
+        if (rotateInput == Vector2.zero) return;
+
+        Vector3 yawTorque = transform.up * (rotateInput.x * turnTorque);
+
+        Vector3 camRight = PlayerCamera.Instance.CinemachineCam.transform.right;
+        Vector3 pitchAxis = Vector3.ProjectOnPlane(camRight, transform.up).normalized;
+        Vector3 pitchTorque = pitchAxis * (-rotateInput.y * turnTorque);
+
+        Vector3 totalTorque = yawTorque + pitchTorque;
+
+        if (rb.angularVelocity.magnitude < maxAngularSpeed)
+        {
+            rb.angularVelocity += totalTorque;
+        }
+    }
+
+    Vector3 GetMovementDirection()
+    {
+        Transform camTransform = PlayerCamera.Instance.CinemachineCam.transform;
+
+        Vector2 input = InputManager.MoveInput;
+        Vector3 moveDir = ((camTransform.forward * input.y) + (camTransform.right * input.x)).normalized;
+        
+        return moveDir;
     }
 }
